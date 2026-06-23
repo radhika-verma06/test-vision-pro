20337e0117fa69f2034b79c0d146cb3feeeac1cf
radhika <radhika verma>
2026-06-23 14:34:00 +1000

    Initial Commit

---
+++ therapy/therapyApp.swift
@@ -0,0 +1,17 @@
+//
+//  therapyApp.swift
+//  therapy
+//
+//  Created by radhika verma  on 23/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct therapyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
